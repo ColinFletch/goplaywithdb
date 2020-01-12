181a51923cd6bfb5819f0ef29e59e1c9ab44dfd9
Colin Fletcher <colin.fletcher@fiserv.com>
2020-01-12 12:19:09 -0500

    Repo init

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 
 	"github.com/go-chi/chi"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
 var router *chi.Mux
@@ -18,6 +18,7 @@ const (
 )
 
 func routers() *chi.Mux {
+	
 	router.Get("/posts", ViewAllPosts)
 	router.Get("/posts/{id}", ViewPost)
 	router.Post("/posts", CreatePost)
@@ -27,14 +28,34 @@ func routers() *chi.Mux {
 	return router
 }
 
-func init() { 
-    router = chi.NewRouter() 
-    router.Use(middleware.Recoverer)  
-    
-    dbSource := fmt.Sprintf("root:%s@tcp(%s:%s)/%s?charset=utf8",  dbPass, dbHost, dbPort, dbName)
-    
-    var err error
-    db, err = sql.Open("mysql", dbSource) 
-    
-    catch(err)
+func main() {
+	connection, err := driver.ConnectSQL(dbHost, dbPort, "root", dbPass, dbName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+
+	postHandler := ph.NewPostHandler(connection)
+	r.Route("/", func(rt chi.Router) {
+		rt.Mount("/posts", postRouter(postHandler))
+	})
+
+	fmt.Println("Server listen at :8005")
+	http.ListenAndServe(":8005", r)
 }
+
+func postRouter(postHandler *ph.Post) http.Handler {
+	r := chi.NewRouter()
+	r.Get("/", postHandler.Fetch)
+	r.Get("/{id:[0-9]+}", postHandler.GetByID)
+	r.Post("/", postHandler.Create)
+	r.Put("/{id:[0-9]+}", postHandler.Update)
+	r.Delete("/{id:[0-9]+}", postHandler.Delete)
+
+	return r
+}
+
--- repository/repository.go
+++ repository/repository.go
@@ -7,9 +7,9 @@ import (
 
 // PostRepo Interface outlining the methods to create within our Http Handler
 type PostRepo interface {
-	Fetch(ctx context.Context, num int64) ([]*model.Post, error)
-	GetByID(ctx context.Context, id int64) (*model.Post, error)
-	Create(ctx context.Context, p *model.Post) (int64, error)
-	Update(ctx context.Context, p *model.Post) (*model.Post, error)
-	Delete(ctx context.Context, id int64) (bool, error)
+	ViewAllPosts(ctx context.Context, num int32) ([]*model.Post, error)
+	ViewPost(ctx context.Context, id int32) (*model.Post, error)
+	CreatePost(ctx context.Context, p *model.Post) (int64, error)
+	UpdatePost(ctx context.Context, p *model.Post) (*model.Post, error)
+	DeletePost(ctx context.Context, id int32) (bool, error)
 }
