03f7ad7db82a6fd70ea3391d2de733e390898981
Colin Fletcher <colin.fletcher@fiserv.com>
2020-01-15 19:27:21 -0500

    repository refactor

--- repository/repository.go
+++ repository/repository.go
@@ -11,5 +11,5 @@ type PostRepo interface {
 	ViewPost(ctx context.Context, id int32) (*model.Post, error)
 	CreatePost(ctx context.Context, p *model.Post) (int64, error)
 	UpdatePost(ctx context.Context, p *model.Post) (*model.Post, error)
-	DeletePost(ctx context.Context, id int32) (bool, error)
+	//DeletePost(ctx context.Context, id int32) (bool, error)
 }
